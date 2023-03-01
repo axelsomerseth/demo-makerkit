096cd71c63b9dc52afd116d0356a874f0cae4a00
anme-22 <angeldavidmendez2012@gmail.com>
2023-03-01 15:16:55 -0600

    Adding useCompleteTask hook

---
+++ app/lib/tasks/hooks/use-complete-task.ts
@@ -0,0 +1,36 @@
+import { doc, updateDoc } from 'firebase/firestore';
+import { useCallback } from 'react';
+import { useFirestore } from 'reactfire';
+import useRequestState from '~/core/hooks/use-request-state';
+import { TASKS_COLLECTION } from '~/lib/firestore-collections';
+
+import type { Task } from '../types/task';
+import type { FirebaseError } from 'firebase/app';
+
+/**
+ * @name useCompleteTask
+ * @description Hook to mark as completed a task
+ */
+function useCompleteTask() {
+  const { state, setData, setError, setLoading } = useRequestState();
+
+  const firestore = useFirestore();
+  const completeTaskCallback = useCallback(
+    async (task: Task) => {
+      setLoading(true);
+      try {
+        const taskDoc = doc(firestore, TASKS_COLLECTION, task.id as string);
+        await updateDoc(taskDoc, { isDone: !task.isDone });
+        setData(task);
+      } catch (error) {
+        setError((error as FirebaseError).message);
+      }
+    },
+    [setData, setError, setLoading]
+  );
+  return [completeTaskCallback, state] as [
+    typeof completeTaskCallback,
+    typeof state
+  ];
+}
+export default useCompleteTask;
