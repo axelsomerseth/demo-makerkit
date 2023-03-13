9261cf92bd0edbd984f44db0a48b30b407610b0f
Axel Somerseth Cordova <axelsomerseth@gmail.com>
2023-03-13 11:25:39 -0600

    fixing import types

--- app/lib/tasks/hooks/use-complete-task.ts
+++ app/lib/tasks/hooks/use-complete-task.ts
@@ -1,10 +1,11 @@
 import { useCallback } from 'react';
 import { useFirestore } from 'reactfire';
-import { FirebaseError } from 'firebase/app';
 import { doc, updateDoc } from 'firebase/firestore';
 import useRequestState from '~/core/hooks/use-request-state';
 import { TASKS_COLLECTION } from '~/lib/firestore-collections';
-import { Task } from '../types/task';
+
+import type { FirebaseError } from 'firebase/app';
+import type { Task } from '../types/task';
 
 function useCompleteTask() {
   const { setData, setError, setLoading, state } = useRequestState();
